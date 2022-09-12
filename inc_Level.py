54e0f9aab7aec185ca6284caa950a1543c8fb44f
JamesW3211 <108100056+JamesW3211@users.noreply.github.com>
2022-09-12 19:41:18 -0400

    Add files via upload

---
+++ inc_Level.py
@@ -0,0 +1,79 @@
+import pygame
+import random
+import json
+
+from inc_SpriteSheet import SpriteSheet
+from inc_Sprite import Sprite
+
+
+
+
+class Level(object):
+
+    def __init__(self):
+        super().__init__()
+
+        # Distance is used to go through the script
+        self.distance = 0
+        self.distance_timer = pygame.time.get_ticks()
+
+        # Load the script [from file]
+        self.script = {
+            201: {'name': "BACKGROUND", 'type': 11, 'x': 0, 'y': 0},
+            200: {'name': "ENEMY", 'type': 10, 'x': 50, 'y': -50},
+            350: {'name': "ENEMY", 'type': 10, 'x': 100, 'y': -50},
+            300: {'name': "ENEMY", 'type': 10, 'x': 120, 'y': -50},
+            420: {'name': "ENEMY", 'type': 10, 'x': 160, 'y': -50},
+            440: {'name': "ENEMY", 'type': 10, 'x': 190, 'y': -50},
+            460: {'name': "ENEMY", 'type': 10, 'x': 210, 'y': -50},
+
+            800: {'name': "ENEMY", 'type': 10, 'x': 50, 'y': -50},
+            850: {'name': "ENEMY", 'type': 10, 'x': 100, 'y': -50},
+            900: {'name': "ENEMY", 'type': 10, 'x': 120, 'y': -50},
+            920: {'name': "ENEMY", 'type': 10, 'x': 160, 'y': -50},
+            940: {'name': "ENEMY", 'type': 10, 'x': 190, 'y': -50},
+            960: {'name': "ENEMY", 'type': 10, 'x': 210, 'y': -50},
+
+        }
+        # Flag variables signal when to spawn things outside of the level handler
+        self.enemy_flag = False
+        self.background_flag = False
+
+        # Load the sprite sheet, depending on the "type"
+
+        # Unique objects
+        self.objects = pygame.sprite.Group()
+
+    def increment(self):
+        # Increment the distance, which is the key of the script dictionary
+        if pygame.time.get_ticks() > self.distance_timer + 1:
+            self.distance_timer = pygame.time.get_ticks()
+            self.distance += 1
+
+        if self.distance in self.script:
+            print("activated: ", self.script[self.distance])
+            # Handle Script Keywords
+            if self.script[self.distance]["name"] == "ENEMY":
+                self.enemy_flag = self.script[self.distance]
+            if self.script[self.distance]["name"] == "BACKGROUND":
+                self.background_flag = True
+            del (self.script[self.distance])
+
+
+
+
+    '''  Update
+        Handles animations and gun timing
+    '''
+
+    def update(self):
+
+        self.objects.update()
+
+
+    def draw(self, win):
+
+        self.objects.draw(win)
+
+
+
---
+++ inc_Planet.py
@@ -0,0 +1,38 @@
+import random
+
+import pygame
+
+
+
+class Planet(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Planet, self).__init__()
+        self.planet_01 = pygame.image.load('assets/Images/planet2.png').convert_alpha()
+        self.planet_02 = pygame.image.load('assets/Images/planet2.png').convert_alpha()
+        self.planet_03 = pygame.image.load('assets/Images/planet3.png').convert_alpha()
+        self.planet_04 = pygame.image.load('assets/Images/planet2.png').convert_alpha()
+        self.planet_05 = pygame.image.load('assets/Images/planet3.png').convert_alpha()
+        self.img_planets = [self.planet_01,
+                            self.planet_02,
+                            self.planet_03,
+                            self.planet_04,
+                            self.planet_05]
+        self.num_planets = len(self.img_planets)
+        self.img_index = random.randrange(0, self.num_planets - 1)
+        self.image = self.img_planets[self.img_index]
+        self.scale_value = random.uniform(0.75, 1.5)
+        self.image = pygame.transform.scale(self.image, (int(self.image.get_width()* self.scale_value),
+                                            int(self.image.get_width()* self.scale_value)))
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 240 - self.image.get_width())
+        self.rect.y = 0 - self.rect.height
+        self.pos_x = random.randrange(0, 240 - self.image.get_width())
+        self.pos_y = 0 - self.rect.height
+        self.vel_x = 0.0
+        self.vel_y = random.uniform(0.2,0.4)
+
+    def update(self):
+        self.pos_x += self.vel_x
+        self.pos_y += self.vel_y
+        self.rect.x = int(self.pos_x)
+        self.rect.y = int(self.pos_y)
